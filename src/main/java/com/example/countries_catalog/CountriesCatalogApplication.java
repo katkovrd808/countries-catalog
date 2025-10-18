1a3ce4eef38587b5e5e150a6306c652deed91234
Roman Katkov <romangadle@gmail.com>
2025-10-19 00:07:44 +0700

    Initial commit

---
+++ src/main/java/com/example/countries_catalog/CountriesCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.countries_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/countries_catalog/CountriesCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countries_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
